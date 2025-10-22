c903b7e67021a5160faa743b4a78cdce98ba76f0
Systemized <VincentNguyen.XLV@gmail.com>
2025-10-22 15:15:53 -0400

    updated database helper/models, and updated adding vehicle page

---
+++ lib/database/database_helper.dart
@@ -0,0 +1,107 @@
+import 'package:path/path.dart';		
+import 'package:sqflite/sqflite.dart';	
+import 'package:path_provider/path_provider.dart';
+import 'models/vehicle.dart';
+
+class DatabaseHelper {	
+  static const _databaseName = "VehicleDatabase.db";	
+  static const _databaseVersion = 1;	
+
+  static const table = 'vehicles';	
+
+  static const columnId = 'id';	
+  static const columnVin = 'vin';	
+  static const columnMake = 'make';
+  static const columnModel = 'model';
+  static const columnYear = 'year';
+  static const columnEngine = 'engine';
+  static const columnCreatedAt = 'createdAt';
+
+  late Database _db;	
+
+  // this opens the database (and creates it if it doesn't exist)	
+  Future<void> init() async {	
+    final documentsDirectory = await getApplicationDocumentsDirectory();	
+    final path = join(documentsDirectory.path, _databaseName);	
+    _db = await openDatabase(	
+      path,	
+      version: _databaseVersion,	
+      onCreate: _onCreate,	
+    );	
+  }	
+
+  // SQL code to create the database table	
+  Future _onCreate(Database db, int version) async {	
+    await db.execute('''	
+          CREATE TABLE $table (	
+            $columnId INTEGER PRIMARY KEY AUTOINCREMENT,	
+            $columnVin TEXT UNIQUE NOT NULL,	
+            $columnMake TEXT NOT NULL,
+            $columnModel TEXT NOT NULL,
+            $columnYear TEXT NOT NULL,
+            $columnEngine TEXT NOT NULL,
+            $columnCreatedAt TEXT NOT NULL
+          )	
+          ''');	
+  }	
+
+  // Helper methods	
+
+  // Inserts a vehicle in the database
+  Future<int> insertVehicle(Vehicle vehicle) async {	
+    try {
+      return await _db.insert(table, vehicle.toMap());	
+    } catch (e) {
+      throw Exception('Vehicle with this VIN already exists');
+    }
+  }	
+
+  // Gets all vehicles from the database
+  Future<List<Vehicle>> getAllVehicles() async {	
+    final List<Map<String, dynamic>> maps = await _db.query(table, orderBy: '$columnCreatedAt DESC');
+    return List.generate(maps.length, (i) => Vehicle.fromMap(maps[i]));
+  }	
+
+  // Gets a vehicle by VIN
+  Future<Vehicle?> getVehicleByVin(String vin) async {
+    final List<Map<String, dynamic>> maps = await _db.query(
+      table,
+      where: '$columnVin = ?',
+      whereArgs: [vin],
+    );
+    if (maps.isNotEmpty) {
+      return Vehicle.fromMap(maps.first);
+    }
+    return null;
+  }
+
+  // Gets the count of vehicles
+  Future<int> getVehicleCount() async {	
+    final results = await _db.rawQuery('SELECT COUNT(*) FROM $table');	
+    return Sqflite.firstIntValue(results) ?? 0;	
+  }	
+
+  // Updates a vehicle
+  Future<int> updateVehicle(Vehicle vehicle) async {	
+    return await _db.update(	
+      table,	
+      vehicle.toMap(),	
+      where: '$columnId = ?',	
+      whereArgs: [vehicle.id],	
+    );	
+  }	
+
+  // Deletes a vehicle by id
+  Future<int> deleteVehicle(int id) async {	
+    return await _db.delete(	
+      table,	
+      where: '$columnId = ?',	
+      whereArgs: [id],	
+    );	
+  }
+
+  // Closes the database
+  Future<void> close() async {
+    await _db.close();
+  }	
+}	
--- lib/pages/new_vehicle_page.dart
+++ lib/pages/new_vehicle_page.dart
@@ -1,7 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
-class NewVehiclePage extends StatelessWidget {
+class NewVehiclePage extends StatefulWidget {
   const NewVehiclePage({super.key});
+
+  @override
+  State<NewVehiclePage> createState() => _NewVehiclePageState();
+}
+
+class _NewVehiclePageState extends State<NewVehiclePage> {
+  final TextEditingController _vinController = TextEditingController();
+  bool _isValidVin = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _vinController.addListener(_validateVin);
+  }
+
+  @override
+  void dispose() {
+    _vinController.dispose();
+    super.dispose();
+  }
+
+  void _validateVin() {
+    setState(() {
+      _isValidVin = _vinController.text.length == 16;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +40,26 @@ class NewVehiclePage extends StatelessWidget {
           Text('Add New Vehicle', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
           SizedBox(height: 20),
           TextField(
-            decoration: const InputDecoration(
-              border: OutlineInputBorder(),
-              labelText: 'Vin Number',
+            controller: _vinController,
+            maxLength: 16,
+            textCapitalization: TextCapitalization.characters,
+            inputFormatters: [
+              FilteringTextInputFormatter.allow(RegExp(r'[a-zA-Z0-9]')),
+            ],
+            decoration: InputDecoration(
+              border: const OutlineInputBorder(),
+              labelText: 'VIN Number',
+              hintText: 'Enter VIN number',
+              counterText: '${_vinController.text.length}',
+              errorText: _vinController.text.isNotEmpty && _vinController.text.length != 16
+                  ? 'Enter 16 Chracters'
+                  : null,
             ),
+            onChanged: (value) {
+              setState(() {
+                _isValidVin = value.length == 17;
+              });
+            },
           ),
           SizedBox(height: 20),
 
@@ -24,10 +67,14 @@ class NewVehiclePage extends StatelessWidget {
           ElevatedButton(
             style: ElevatedButton.styleFrom(
               padding: EdgeInsets.symmetric(horizontal: 75, vertical: 25),
-              backgroundColor: Colors.blue[400],
+              backgroundColor: _isValidVin ? Colors.blue[400] : Colors.grey[400],
             ),
-            onPressed: () {
-            },
+            onPressed: _isValidVin ? () {
+              // TODO: Add vehicle logic here
+              ScaffoldMessenger.of(context).showSnackBar(
+                SnackBar(content: Text('Vehicle added successfully!')),
+              );
+            } : null,
             child: Text('Add Vehicle', style: TextStyle(color: Colors.white)),
           ),
 
