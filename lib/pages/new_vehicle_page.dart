7d208482ee913bc54fa162e030c9264b6fcf32ea
Systemized <VincentNguyen.XLV@gmail.com>
2025-10-22 14:19:08 -0400

    updated new_vehicle_page

--- lib/pages/new_vehicle_page.dart
+++ lib/pages/new_vehicle_page.dart
@@ -5,8 +5,36 @@ class NewVehiclePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("Add New Vehicle", style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 60.0),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Text('Add New Vehicle', style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),
+          SizedBox(height: 20),
+          TextField(
+            decoration: const InputDecoration(
+              border: OutlineInputBorder(),
+              labelText: 'Vin Number',
+            ),
+          ),
+          SizedBox(height: 20),
+
+          SizedBox(height: 40),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              padding: EdgeInsets.symmetric(horizontal: 75, vertical: 25),
+              backgroundColor: Colors.blue[400],
+            ),
+            onPressed: () {
+            },
+            child: Text('Add Vehicle', style: TextStyle(color: Colors.white)),
+          ),
+
+        ],
+      ),
     );
   }
 }
+
+
